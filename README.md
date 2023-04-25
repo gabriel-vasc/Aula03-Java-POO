80cb1dac84b04aa9567432e868d35e135103cba3
Gabriel Vasconcelos <gabrielvasc98@outlook.com>
2023-04-25 19:19:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aula03-Java-POO
+ Nessa aula revisei funcionamento de modificadores de visibilidade em java no contexto de Orientação a Objetos.
---
+++ src/aula02/Caneta.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula02;
+
+/**
+ *
+ * @author gabri
+ */
+public class Caneta {
+    
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    
+    
+    public void status() {
+        
+        System.out.println("Modelo: "+this.modelo);
+        System.out.println("Uma caneta "+this.cor);
+        System.out.println("Ponta: "+this.ponta);
+        System.out.println("Carga: "+this.carga);
+        System.out.println("está tampada? "+this.tampada);
+        
+    }
+    
+    public void rabiscar() {
+        
+        if (this.tampada == true) {
+            System.out.println("Erro! Não posso rabiscar.\n");
+        } else {
+            System.out.println("Estou rabiscando.\n");
+        }
+        
+    }    
+    
+    public void tampar() {
+        
+        this.tampada = true;
+        
+    }
+    
+    public void destampar() {
+        
+        this.tampada = false;
+        
+    }
+    
+}
